8127b0d496d7777cd911e43a0f33731844b27fdc
Jesse Hattabaugh <arkanciscan@gmail.com>
2013-08-27 23:06:45 -0400

    socket.io

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ An HTML5 Canvas based game that's playable on the 3DS
 
 How to play
 
-Use the arrow keys to move your guy around a top down grid-based map. Draw patterns 
+Use the arrow keys to move your guy around a top down grid-based map.
+Copy any map tile to your palette
+Fill empty map tiles with other tiles you've found
--- server.js
+++ server.js
@@ -1,4 +1,48 @@
-var app = require('harp').server(__dirname, { port: process.env.PORT || 5000 }),
-    io = require('socket.io').listen(app);
+//var app = require('harp').server(__dirname, { port: process.env.PORT || 5000 }),
+//    io = require('socket.io').listen(app);
+    
+/*    Harp.io 
+var express = require("express");
+var harp = require("harp");
+var app = express();
+
+app.configure(function(){
+    app.use(express.static(__dirname + "/public"));
+    app.use(harp.pipeline(__dirname + "/public"));
+});
+
+// routes as normal
+*/
+
+/* Socket.io
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+
+app.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+*/
+
+var express = require("express"),
+    harp = require("harp"),
+    app = express(),
+    io = require("socket.io").listen(app),
+    port = process.env.PORT || 5000;
+
+app.configure(function(){
+    app.use(express.static(__dirname + "/public"));
+    app.use(harp.pipeline(__dirname + "/public"));
+});
+
+app.listen(port);
     
 console.log("Server started");
