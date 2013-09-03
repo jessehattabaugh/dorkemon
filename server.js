e43c52c98f78be32e1618f9512853a7b9245b3b0
Jesse Hattabaugh <arkanciscan@gmail.com>
2013-09-02 20:30:38 -0400

    hmm, don't think this works on 3DS very well

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
+//var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
     console.log("serving the client.html");
@@ -15,26 +15,26 @@ console.log("Server started on port" + PORT);
 /* Socket.io Stuff ***********************************************************/
 
 // Heroku compatibility
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
-});
+//io.configure(function () { 
+//  io.set("transports", ["xhr-polling"]);
+//  io.set("polling duration", 10);
+//});
 
-io.on('connection', function(socket){
-    console.log("Connection started.");
-    
-    socket.on('join', function(data){
-        //console.log(data);
-        io.sockets.emit('joined', data);
-    });
-    
-    socket.on('say', function(data){
-        //console.dir(data);
-        io.sockets.emit('hear', data);
-    });
-    
-    socket.on('disconnect', function(data) {
-        //console.log("Connection stopped.");
-        io.sockets.emit('left', data);
-    });
-});
+//io.on('connection', function(socket){
+//    console.log("Connection started.");
+//    
+//    socket.on('join', function(data){
+//        //console.log(data);
+//        io.sockets.emit('joined', data);
+//    });
+//    
+//    socket.on('say', function(data){
+//        //console.dir(data);
+//        io.sockets.emit('hear', data);
+//    });
+//    
+//    socket.on('disconnect', function(data) {
+//        //console.log("Connection stopped.");
+//        io.sockets.emit('left', data);
+//    });
+//});
